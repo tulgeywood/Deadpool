5cd66f231c6d55116879439684805c41c2c1a28a
jesses <jesses@aweber.com>
2015-08-15 02:48:18 -0400

    Installation scritp

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+curl -s https://raw.githubusercontent.com/tulgeywood/Deadpool/master/wade.sh \
+-o /Library/Application\ Support/JAMF/ManagementFrameworkScripts/wade.sh
+chmod +x /Library/Application\ Support/JAMF/ManagementFrameworkScripts/wade.sh
+
+curl -s https://raw.githubusercontent.com/tulgeywood/Deadpool/master/fourthWall.sh \
+-o /Library/Application\ Support/JAMF/ManagementFrameworkScripts/fourthWall.sh
+chmod +x /Library/Application\ Support/JAMF/ManagementFrameworkScripts/fourthWall.sh
+
+curl -s https://raw.githubusercontent.com/tulgeywood/Deadpool/master/com.jamfsoftware.fourthWall.plist \
+-o /Library/LaunchDaemons/com.jamfsoftware.fourthWall.plist
+launchctl load /Library/LaunchDaemons/com.jamfsoftware.fourthWall.plist
+
+jamf manage
