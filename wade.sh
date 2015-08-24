859dceea3314245988eb61f4b8f2f65e0c8c2323
tulgeywood <tulgeywood@mac.com>
2015-08-24 11:40:52 -0400

    Single quotes are your friend

--- wade.sh
+++ wade.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #logging function
 logOutput(){
-	$1 | xargs -I{} printf '%s %s\n' "$(date '+%b %d %H:%M:%S')" "{}" >> /var/log/jamfv.log
+	$1 | xargs -I{} printf '%s %s\n' "$(date '+%b %d %H:%M:%S')" '{}' >> /var/log/jamfv.log
 }
 #set some variables
 startTime=$(date +%s)
